97ba989f6da65e651760a4a0f2bb0ef8f4e463be
Tawhidur Noor <56801563+tawhidurnoor@users.noreply.github.com>
2021-12-07 03:43:48 +0600

    Seeder

--- database/seeds/RolePermissionSeeder.php
+++ database/seeds/RolePermissionSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class RolePermissionSeeder extends Seeder
 {
@@ -11,6 +13,55 @@ class RolePermissionSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //we are basically creating permissions and assigning theme to suoer user
+
+        // Create Roles
+        $roleSuperAdmin = Role::create(['name' => 'superadmin']);
+        $roleAdmin = Role::create(['name' => 'admin']);
+        $roleEditor = Role::create(['name' => 'editor']);
+        $roleUser = Role::create(['name' => 'user']);
+
+
+        // Permission List as array
+        $permissions = [
+
+            // Dashboard
+            'dashboard.view',
+
+            // Blog Permissions
+            'blog.create',
+            'blog.view',
+            'blog.edit',
+            'blog.delete',
+            'blog.approve',
+
+            // Admin Permissions
+            'admin.create',
+            'admin.view',
+            'admin.edit',
+            'admin.delete',
+            'admin.approve',
+
+            // Role Permissions
+            'role.create',
+            'role.view',
+            'role.edit',
+            'role.delete',
+            'role.approve',
+
+            // Profile Permissions
+            'profile.view',
+            'profile.edit'
+        ];
+
+
+        // Create and Assign Permissions
+        // 
+        for ($i = 0; $i < count($permissions); $i++) {
+            // Create Permission
+            $permission = Permission::create(['name' => $permissions[$i]]);
+            $roleSuperAdmin->givePermissionTo($permission);
+            //$permission->assignRole($roleSuperAdmin);
+        }
     }
 }
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -11,6 +13,13 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $user = User::where('email', 'tawhidbadhan@gmail.com')->first();
+        if (is_null($user)) {
+            $user = new User();
+            $user->name = "Tawhidur Noor";
+            $user->email = "tawhidbadhan@gmail.com";
+            $user->password = Hash::make('12345678');
+            $user->save();
+        }
     }
 }
