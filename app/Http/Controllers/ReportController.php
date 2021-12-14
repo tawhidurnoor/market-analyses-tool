7f73a40962e2001329784312066798a5c4c80109
Tawhidur Noor <56801563+tawhidurnoor@users.noreply.github.com>
2021-12-15 02:43:11 +0600

    popular Products of Last 30 days

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -35,6 +35,16 @@ class ReportController extends Controller
             ->orderBy('sale_ammount', 'desc')
             ->first();
 
-        return view('report.index', compact('most_sold_product', 'most_sold_subcategory', 'most_sold_category'));
+        $popular_products = Sale::where('sales.created_at', '>=', $date)
+            ->join('products', 'sales.product_id', 'products.id')
+            ->join('product_subcategories', 'products.product_subcat_id', 'product_subcategories.id')
+            ->join('product_categories', 'product_subcategories.category_id', 'product_categories.id')
+            ->select('products.product_name as product_name', DB::raw('SUM(sale_ammount) as sale_ammount'), 'product_subcategories.subcategory_name as subcategory_name', 'product_categories.category_name as category_name')
+            ->groupByRaw('product_name, category_name, subcategory_name')
+            ->orderBy('sale_ammount', 'desc')
+            ->take(10)
+            ->get();
+
+        return view('report.index', compact('most_sold_product', 'most_sold_subcategory', 'most_sold_category', 'popular_products'));
     }
 }
--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -9,6 +9,7 @@
 <!-- Start Status area -->
 <div class="notika-status-area">
     <div class="container">
+
         <div class="row">
             <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
                 <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30" style="height: 150px;">
@@ -38,9 +39,44 @@
                 </div>
             </div>
         </div>
+
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="normal-table-list mg-t-30">
+                    <h2 class="text-info" style="font-size: 21px;">Popular Products</h2>
+                    <p>Most sold products of last <strong>30</strong> days.</p>
+                    <div class="bsc-tbl-st">
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Product</th>
+                                    <th>Subcategory</th>
+                                    <th>Category</th>
+                                    <th>Sold Unit</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($popular_products as $p_p)
+                                <tr>
+                                    <td> {{$loop->index+1}} </td>
+                                    <td> {{$p_p->product_name}} </td>
+                                    <td> {{$p_p->subcategory_name}} </td>
+                                    <td> {{$p_p->category_name}} </td>
+                                    <td> {{$p_p->sale_ammount}} </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
 <!-- End Status area-->
+
 
 @endsection
 
