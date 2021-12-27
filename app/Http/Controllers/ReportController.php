80d7e60de036ec556c2f77259bb3785835240798
Tawhidur Noor <56801563+tawhidurnoor@users.noreply.github.com>
2021-12-27 21:04:32 +0600

    Algorithm Improvement in Trending Products

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -18,14 +18,32 @@ class ReportController extends Controller
 
     public function trending()
     {
-        $date = Carbon::now()->subDays(7);
+        //$date = Carbon::now()->subDays(7);
 
+        /*
         $most_sold_product = Sale::where('sales.created_at', '>=', $date)
             ->join('products', 'sales.product_id', 'products.id')
             ->selectRaw('sum(sale_ammount) as sale_ammount, products.product_name as product_name')
             ->groupBy('product_name')
             ->orderBy('sale_ammount', 'desc')
             ->first();
+            */
+
+        $most_sold_product = [];
+        $i = 7;
+        //looping till we get a non null value, date is updated through this wile loop for all entries
+        while (!$most_sold_product) {
+            $date = Carbon::now()->subDays($i);
+
+            $most_sold_product = Sale::where('sales.created_at', '>=', $date)
+                ->join('products', 'sales.product_id', 'products.id')
+                ->selectRaw('sum(sale_ammount) as sale_ammount, products.product_name as product_name')
+                ->groupBy('product_name')
+                ->orderBy('sale_ammount', 'desc')
+                ->first();
+            $i++;
+        }
+
         $most_sold_subcategory = Sale::where('sales.created_at', '>=', $date)
             ->join('products', 'sales.product_id', 'products.id')
             ->join('product_subcategories', 'products.product_subcat_id', 'product_subcategories.id')
@@ -42,15 +60,22 @@ class ReportController extends Controller
             ->orderBy('sale_ammount', 'desc')
             ->first();
 
-        $popular_products = Sale::where('sales.created_at', '>=', $date)
-            ->join('products', 'sales.product_id', 'products.id')
-            ->join('product_subcategories', 'products.product_subcat_id', 'product_subcategories.id')
-            ->join('product_categories', 'product_subcategories.category_id', 'product_categories.id')
-            ->select('products.product_name as product_name', DB::raw('SUM(sale_ammount) as sale_ammount'), 'product_subcategories.subcategory_name as subcategory_name', 'product_categories.category_name as category_name')
-            ->groupByRaw('product_name, category_name, subcategory_name')
-            ->orderBy('sale_ammount', 'desc')
-            ->take(10)
-            ->get();
+        $popular_products = [];
+        $i = 30;
+        while (!$popular_products) {
+
+            $date = Carbon::now()->subDays($i);
+            $popular_products = Sale::where('sales.created_at', '>=', $date)
+                ->join('products', 'sales.product_id', 'products.id')
+                ->join('product_subcategories', 'products.product_subcat_id', 'product_subcategories.id')
+                ->join('product_categories', 'product_subcategories.category_id', 'product_categories.id')
+                ->select('products.product_name as product_name', DB::raw('SUM(sale_ammount) as sale_ammount'), 'product_subcategories.subcategory_name as subcategory_name', 'product_categories.category_name as category_name')
+                ->groupByRaw('product_name, category_name, subcategory_name')
+                ->orderBy('sale_ammount', 'desc')
+                ->take(10)
+                ->get();
+            $i++;
+        }
 
         return view('report.trending', compact('most_sold_product', 'most_sold_subcategory', 'most_sold_category', 'popular_products'));
     }
