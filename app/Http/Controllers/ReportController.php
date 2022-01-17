7e370444673e7063055b1619539c903576d35c3a
Tawhidur Noor <56801563+tawhidurnoor@users.noreply.github.com>
2022-01-18 04:25:40 +0600

    Analysis - Subcategory

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -259,30 +259,118 @@ class ReportController extends Controller
                     //     ->orderBy('sale_ammount', 'desc')
                     //     ->first();
 
-                    // $most_sold_district = Sale::where('product_id', $request->product_id)
-                    //     ->join('cities', 'sales.city_id', 'cities.id')
-                    //     ->join('districts', 'cities.district_id', 'districts.id')
-                    //     ->groupBy('districts.district_name')
-                    //     ->selectRaw('sum(sales.sale_ammount) as sale_ammount, districts.district_name as district_name')
-                    //     ->whereBetween(
-                    //         'date',
-                    //         [Carbon::now()->subMonth(6), Carbon::now()]
-                    //     )
-                    //     ->orderBy('sale_ammount', 'desc')
-                    //     ->first();
+                    $most_sold_district = Sale::whereIn('product_id', $product_ids)
+                        ->join('cities', 'sales.city_id', 'cities.id')
+                        ->join('districts', 'cities.district_id', 'districts.id')
+                        ->groupBy('districts.district_name')
+                        ->selectRaw('sum(sales.sale_ammount) as sale_ammount, districts.district_name as district_name')
+                        ->whereBetween(
+                            'date',
+                            [Carbon::now()->subMonth(6), Carbon::now()]
+                        )
+                        ->orderBy('sale_ammount', 'desc')
+                        ->first();
 
-                    // $most_sold_division = Sale::where('product_id', $request->product_id)
-                    //     ->join('cities', 'sales.city_id', 'cities.id')
-                    //     ->join('districts', 'cities.district_id', 'districts.id')
-                    //     ->join('divisions', 'districts.division_id', 'divisions.id')
-                    //     ->groupBy('divisions.division_name')
-                    //     ->selectRaw('sum(sales.sale_ammount) as sale_ammount, divisions.division_name as division_name')
+                    $most_sold_division = Sale::whereIn('product_id', $product_ids)
+                        ->join('cities', 'sales.city_id', 'cities.id')
+                        ->join('districts', 'cities.district_id', 'districts.id')
+                        ->join('divisions', 'districts.division_id', 'divisions.id')
+                        ->groupBy('divisions.division_name')
+                        ->selectRaw('sum(sales.sale_ammount) as sale_ammount, divisions.division_name as division_name')
+                        ->whereBetween(
+                            'date',
+                            [Carbon::now()->subMonth(6), Carbon::now()]
+                        )
+                        ->orderBy('sale_ammount', 'desc')
+                        ->first();
+
+                    $html = '<h3>Sales Statistics</h3>
+                     <p>Sales Statistics of last 6 months(if available)</p>
+                    <div class="row">
+                        <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+                            <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30" style="height: 150px;">
+                                <div class="website-traffic-ctn">
+                                    <h2><span class="counter">' . $most_sold_product->sale_ammount . '</span> Units</h2>
+                                    <h3 class="text-success">' . $most_sold_product->product_name . '</h3>
+                                    <p>Most Sold in ihis Subcategory</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+                            <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30" style="height: 150px;">
+                                <div class="website-traffic-ctn">
+                                    <h2><span class="counter">' . $most_sold_district->sale_ammount . '</span> Units</h2>
+                                    <h3 class="text-primary">' . $most_sold_district->district_name . '</h3>
+                                    <p>Most Sold in ihis District</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+                            <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30 dk-res-mg-t-30" style="height: 150px;">
+                                <div class="website-traffic-ctn">
+                                    <h2><span class="counter">' . $most_sold_division->sale_ammount . '</span> Units</h2>
+                                    <h3 class="text-danger">' . $most_sold_division->division_name . '</h3>
+                                    <p>Most Sold in ihis Division</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>';
+
+                    // $html .= '<br><br>
+                    //         <div class="row">
+                    //             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    //                 <div class="bar-chart-wp">
+                    //                     <canvas height="100vh" id="barchart"></canvas>
+                    //                 </div>
+                    //             </div>
+                    //         </div>';
+
+                    // $six_month_sales_report = Sale::where('product_id', $request->product_id)
+                    //     ->select(DB::raw("(sum(sale_ammount)) as sale_ammount"), DB::raw("DATE_FORMAT(date, '%M-%Y') as month"))
                     //     ->whereBetween(
                     //         'date',
                     //         [Carbon::now()->subMonth(6), Carbon::now()]
                     //     )
-                    //     ->orderBy('sale_ammount', 'desc')
-                    //     ->first();
+                    //     ->groupBy(DB::raw("DATE_FORMAT(date, '%M-%Y')"))
+                    //     ->get();
+
+                    return [
+                        'html' => $html,
+                        'data' => null,
+                    ];
+                } else {
+                    //Product and Subcat not selected but cat is selected
+                    //product_cat_id
+                    $subcat = ProductSubcategory::where('category_id', $request->product_cat_id)
+                        ->select('id')
+                        ->get();
+                    $subcat_ids = [];
+                    foreach ($subcat as $s) {
+                        array_push($subcat_ids, $s->id);
+                    }
+
+                    $products = Product::whereIn('product_subcat_id', $subcat_ids)
+                        ->select('id')
+                        ->get();
+
+                    $product_ids = [];
+
+                    foreach ($products as $p) {
+                        array_push($product_ids, $p->id);
+                    }
+
+                    $most_sold_product = Sale::whereIn('product_id', $product_ids)
+                        ->join('products', 'sales.product_id', 'products.id')
+                        ->selectRaw('sum(sales.sale_ammount) as sale_ammount, products.product_name as product_name')
+                        ->whereBetween(
+                            'date',
+                            [Carbon::now()->subMonth(6), Carbon::now()]
+                        )
+                        ->groupBy('product_name')
+                        ->orderBy('sale_ammount', 'desc')
+                        ->first();
+
+
 
                     $html = '<h3>Sales Statistics</h3>
                      <p>Sales Statistics of last 6 months(if available)</p>
@@ -316,32 +404,8 @@ class ReportController extends Controller
                         </div>
                     </div>';
 
-                    // $html .= '<br><br>
-                    //         <div class="row">
-                    //             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    //                 <div class="bar-chart-wp">
-                    //                     <canvas height="100vh" id="barchart"></canvas>
-                    //                 </div>
-                    //             </div>
-                    //         </div>';
-
-                    // $six_month_sales_report = Sale::where('product_id', $request->product_id)
-                    //     ->select(DB::raw("(sum(sale_ammount)) as sale_ammount"), DB::raw("DATE_FORMAT(date, '%M-%Y') as month"))
-                    //     ->whereBetween(
-                    //         'date',
-                    //         [Carbon::now()->subMonth(6), Carbon::now()]
-                    //     )
-                    //     ->groupBy(DB::raw("DATE_FORMAT(date, '%M-%Y')"))
-                    //     ->get();
-
                     return [
                         'html' => $html,
-                        'data' => null,
-                    ];
-                } else {
-                    //Product and Subcat not selected but cat is selected
-                    return [
-                        'html' => 'Product and Subcat not selected but cat is selected',
                         'data' => [],
                     ];
                 }
